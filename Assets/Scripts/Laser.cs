7cd0f87095e788fd6eec10ab25682bb8cdccb6fc
Rifatul Islam <shovonis09@gmail.com>
2019-11-27 00:10:37 -0600

    init

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float _speed = 8.0f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        MoveUp();
+    }
+
+    void MoveUp()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 8.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float _speed = 3.5f;
+
+    private Vector3 _position;
+
+    [SerializeField]
+    private GameObject _laserObject;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+        CreateLaser();
+    }
+
+    private void CreateLaser()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserObject, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void CalculateMovement()
+    {
+        float getHorizontalInput = Input.GetAxis("Horizontal");
+        float getVerticalInput = Input.GetAxis("Vertical");
+
+        _position = new Vector3(getHorizontalInput, getVerticalInput, 0);
+        transform.Translate(_position * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0));
+
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11.0f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11.0f, transform.position.y, 0);
+        }
+    }
+}
